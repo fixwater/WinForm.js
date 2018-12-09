4a6261697e533a6f9eef5f22aff2e814f3289ce0
HaoDaWang <1306036576@qq.com>
2018-12-09 22:14:07 +0800

    Add header-bar for form

---
+++ web-vue/src/components/form/header-bar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header-bar">
+    <slot/>
+    <div class="control-btns">
+      <ControlBtn type="close"/>
+      <ControlBtn type="min"/>
+      <ControlBtn type="max"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import ControlBtn from "./@control-btn.vue";
+
+@Component({
+  components: {
+    ControlBtn
+  }
+})
+export default class HeaderBar extends Vue {
+  @Prop()
+  title!: string;
+}
+</script>
+
+<style lang="scss">
+@import "../../scss/color";
+
+$left-distance: 2px;
+$top-distance: 5px;
+
+.header-bar {
+  width: 100%;
+  padding: $left-distance $top-distance;
+  background: $header-bar-color;
+  text-align: center;
+  position: relative;
+}
+
+.control-btns {
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  margin-right: $left-distance;
+  top: $top-distance;
+  right: $left-distance;
+}
+</style>
