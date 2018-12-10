cd0b9eea779c798e2ee926bd1fe8e116046b60cf
HaoDaWang <1306036576@qq.com>
2018-12-10 21:27:52 +0800

    For drag

---
+++ web-vue/src/components/form/drag-block.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+export default class DragBlock extends Vue {}
+</script>
+
+<style lang="scss">
+</style>
