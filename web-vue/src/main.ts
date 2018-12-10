8df10c45b87a8182b236628ae450f957c494ec7f
HaoDaWang <1306036576@qq.com>
2018-12-10 21:26:46 +0800

    Add font-awesome

--- web-vue/src/main.ts
+++ web-vue/src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./scss/global.scss";
+import "font-awesome/css/font-awesome.min.css";
 
 Vue.config.productionTip = false;
 
