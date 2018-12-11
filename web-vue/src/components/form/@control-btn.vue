d939cdd52c106c8a7fbd15dab4820c342dd776ef
HaoDaWang <1306036576@qq.com>
2018-12-11 10:29:11 +0800

    Update ugly icon

--- web-vue/src/components/form/@control-btn.vue
+++ web-vue/src/components/form/@control-btn.vue
@@ -1,42 +1,47 @@
 <template>
-  <div :class="type"></div>
+  <div :class="[type, 'control-btn']">
+    <i :class="['fa', `fa-${iconType}`]"></i>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Prop, Component } from "vue-property-decorator";
+import { Dict } from "../../types";
 
 type ControlBtnType = "close" | "min" | "max";
+type WindowBtnType = "window-maximize" | "close" | "minus";
+
+const iconMap: Dict<WindowBtnType> = {
+  close: "close",
+  min: "minus",
+  max: "window-maximize"
+};
 
 @Component({})
 export default class ControlBtn extends Vue {
   @Prop()
   type!: ControlBtnType;
+
+  iconType!: WindowBtnType;
+
+  created(): void {
+    this.iconType = iconMap[this.type];
+  }
 }
 </script>
 
 <style lang="scss">
-@import "../../scss/color";
-
-@mixin control-btn($color) {
-  width: 15px;
-  height: 15px;
-  background: $color;
+.control-btn {
+  height: 100%;
+  padding: 0 5px;
   border-radius: 50%;
   margin: 0 5px;
-  border: 1px solid $border-color;
   cursor: pointer;
-}
 
-.close {
-  @include control-btn($control-btn-close);
-}
-
-.min {
-  @include control-btn($control-btn-min);
-}
-
-.max {
-  @include control-btn($control-btn-max);
+  .fa {
+    font-weight: 100;
+    font-size: 16px;
+  }
 }
 </style>
