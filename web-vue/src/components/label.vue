7d205b0ca1b9708d9bff8f6f8fec693398e86390
HaoDaWang <1306036576@qq.com>
2018-12-11 10:37:11 +0800

    Add label component

---
+++ web-vue/src/components/label.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="label">
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import Vue from "vue";
+
+@Component({})
+export default class Label extends Vue {}
+</script>
+
+<style lang="scss">
+.label {
+  margin: 5px;
+}
+</style>
