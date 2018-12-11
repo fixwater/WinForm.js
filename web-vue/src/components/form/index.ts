28490b0b2205fb489cae18c2d18a8339bb78f95f
HaoDaWang <1306036576@qq.com>
2018-12-11 09:27:26 +0800

    Export all modules

--- web-vue/src/components/form/index.ts
+++ web-vue/src/components/form/index.ts
@@ -1,5 +1,4 @@
 import Form from "./form.vue";
 import HeaderBar from "./header-bar.vue";
-import DragBlock from "./drag-block.vue";
 
-export { Form, HeaderBar, DragBlock };
+export { Form, HeaderBar };
