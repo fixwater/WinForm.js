675b7f1476958421e863616492e537e98a2b13a9
jfx787 <lixun7@126com>
2018-12-11 12:37:46 +0800

    更新图片和项目模板

--- hello-winform/Form1.Designer.cs
+++ hello-winform/Form1.Designer.cs
@@ -40,7 +40,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(800, 376);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Hello .NET Core!";
+            this.label1.Text = "Hello WinForm.js !";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // buttonExit
