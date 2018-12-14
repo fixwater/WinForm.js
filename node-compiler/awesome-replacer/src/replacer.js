02a115d9338d9e85fbde30e6b2744b1e1a6c120a
HaoDaWang <1306036576@qq.com>
2018-12-14 18:19:23 +0800

    Add replace template logic

--- node-compiler/awesome-replacer/src/replacer.js
+++ node-compiler/awesome-replacer/src/replacer.js
@@ -1,11 +1,23 @@
-let MATCH_TITLE = /(this.label1.Text\s*=\s*)"[^"]+"/;
+const TARGET_MATCH_TITLE = /(this.label1.Text\s*=\s*)"[^"]+"/;
+const SOURCE_MATCH_TITLE = /<Label(?:\s*[:@-]?\w+="[^"]+?"\s*)*>([^{}]+)<\/Label>/;
 
-function replaceTitle(origin, title) {
-  let originText = origin.toString();
+function findTitleInSource(originContent) {
+  return SOURCE_MATCH_TITLE.exec(originContent)[1];
+}
 
-  return originText.replace(
-    MATCH_TITLE,
-    (_match, capture1) => `${capture1}"${title}"`
+function replaceTitle(sourceContent, targetContent) {
+  sourceContent = sourceContent.toString();
+  targetContent = targetContent.toString();
+
+  let targetTitle = findTitleInSource(sourceContent);
+
+  if (!targetTitle || targetTitle === "") {
+    throw new Error("Please check your source file does exist title");
+  }
+
+  return targetContent.replace(
+    TARGET_MATCH_TITLE,
+    (_match, capture) => `${capture}"${targetTitle}"`
   );
 }
 
