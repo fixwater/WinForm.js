7bc6bfd1e5295f6a0f9b967001e5200a7aec81ea
HaoDaWang <1306036576@qq.com>
2018-12-14 17:51:06 +0800

    Add errro msg

--- node-compiler/awesome-replacer/src/bin.js
+++ node-compiler/awesome-replacer/src/bin.js
@@ -4,4 +4,14 @@ const { argv } = process;
 const args = argv.slice(2, argv.length);
 const filename = args[0];
 
-main(filename);
+if (!filename) {
+  throw new Error("The filename cannot be undefined!");
+}
+
+try {
+  main(filename);
+} catch (e) {
+  throw e;
+}
+
+console.info("Replace successful !!!");
