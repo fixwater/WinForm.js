36d6f6bee8d072363b55b7dabf5a64957bd4fe3c
HaoDaWang <1306036576@qq.com>
2018-12-14 18:19:12 +0800

    Update output path

--- node-compiler/awesome-replacer/src/main.js
+++ node-compiler/awesome-replacer/src/main.js
@@ -1,10 +1,11 @@
 const { readFile, writeFile } = require("./io");
-const { TARGET_FILE_PATH } = require("./constant");
+const { TARGET_FILE_PATH, SOURCE_FILE_PATH, TPL_PATH } = require("./constant");
 const { replaceTitle } = require("./replacer");
 
 exports.main = async function(filename) {
-  let result = await readFile(TARGET_FILE_PATH);
-  let replaceResult = replaceTitle(result, filename);
+  let sourceContent = await readFile(SOURCE_FILE_PATH);
+  let targetContent = await readFile(TPL_PATH);
+  let replaceResult = replaceTitle(sourceContent, targetContent, filename);
 
   await writeFile(TARGET_FILE_PATH, replaceResult);
 };
