9c100df3f7e6f5f5ab1f2bbfd9cf0d3d61adaa6f
HaoDaWang <1306036576@qq.com>
2018-12-14 18:18:33 +0800

    Remove filename argument

--- node-compiler/awesome-replacer/src/bin.js
+++ node-compiler/awesome-replacer/src/bin.js
@@ -1,17 +1,19 @@
 const { main } = require("./main");
 
-const { argv } = process;
-const args = argv.slice(2, argv.length);
-const filename = args[0];
+// const { argv } = process;
+// const args = argv.slice(2, argv.length);
+// const filename = args[0];
 
-if (!filename) {
-  throw new Error("The filename cannot be undefined!");
-}
+// if (!filename) {
+//   throw new Error("The filename cannot be undefined!");
+// }
 
-try {
-  main(filename);
-} catch (e) {
-  throw e;
-}
+(async () => {
+  try {
+    await main();
+  } catch (e) {
+    throw e;
+  }
 
-console.info("Replace successful !!!");
+  console.info("Replace successful !!!");
+})();
