8680edc9f92660b034118000b6bcc7a4a0636dd9
HaoDaWang <1306036576@qq.com>
2018-12-14 18:18:48 +0800

    Add template path

--- node-compiler/awesome-replacer/src/constant.js
+++ node-compiler/awesome-replacer/src/constant.js
@@ -1,7 +1,17 @@
 const path = require("path");
 
+// hard code (TODO)
 const TARGET_FILE_PATH = path.join("..", "output", "Form1.Designer.cs");
+const SOURCE_FILE_PATH = path.join("..", "input", "form1");
+const TPL_PATH = path.join(
+  "..",
+  "template",
+  "winform-hello",
+  "Form1.Designer.cs"
+);
 
 module.exports = {
-  TARGET_FILE_PATH
+  TARGET_FILE_PATH,
+  SOURCE_FILE_PATH,
+  TPL_PATH
 };
